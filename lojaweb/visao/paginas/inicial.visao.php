b8d9e776738934ebcf230abb9ff306f553cec0d4
1831038 <1831038@LABBMAQ13.itp.local>
2019-04-15 10:45:07 -0300

    Lista MVCd concluída! :)

---
+++ lojaweb/visao/paginas/inicial.visao.php
@@ -0,0 +1,31 @@
+
+
+Que bom que você chegou até aqui.
+
+Isso significa que você está no nosso mundo. A gente explica melhor.
+
+No nosso mundo as coisas funcionam de um jeito diferente. Começa pelo nome que damos às coisas. O que os outros chamam de história, nós chamamos de saga. Uma trajetória de vida nós chamamos de arco. Enquanto muitos viram a página, nós permanecemos nela até sentirmos que é hora de virá-la. Deu para sacar a diferença?      
+ 
+Então vamos falar do nosso mundo: a CCXP.                                
+ 
+No nosso mundo, os grandes estúdios e as grandes franquias trazem em primeira mão trailers especiais, cenas exclusivas e os principais astros do momento. Estamos falando de Disney, Warner, Netflix, FOX, Sony e muitos outros.
+
+No nosso mundo você não apenas vive a saga, você fica frente a frente com os criadores delas. No Artists’ Alley, mais de 1700 ilustradores, roteiristas, coloristas e artistas gráficos já comercializaram seus trabalhos, distribuíram autógrafos e interagiram com os fãs.
+
+Mais uma característica especial do nosso mundo é que nele você pode ser o que quiser. A prova disso é que a CCXP é o ambiente ideal para cosplayers expressarem seu hobby e sua arte, além de promover desfiles e um concurso com o maior prêmio da categoria do país. Você pode vir apenas para apreciar ou então para estar na pele do seu personagem favorito. A escolha é toda sua.
+
+Daria ainda para falar dos superauditórios, dos estandes das grandes marcas ou do maior shopping geek do mundo, mas a gente acredita que não dá resumir tudo que acontece no evento de cultura pop mais épico do Brasil apenas em um texto.
+
+
+
+
+
+Porque, afinal, o épico não se escreve. O épico se vive.
+
+CCXP.  Para nós o mundo é assim.
+
+<h1>Aplicação Web com o MVCd</h1>
+
+<p>Este é um projeto desenvolvido com propósito educacional e não deve ser usado em projetos reais. A ideia geral do projeto é apresentar as ideias e conceitos de arquitetura, modelos e framework para alunos que ainda não conhecem a orientação a objetos.</p> 
+
+<p>O framework utiliza o padrão arquitetural MVC, logo sua estrutura básica apresenta os três principais objetos da arquitetura, representados pelas pastas <code>modelo<code>, <code>controlador</code> e <code>visao</code>. </p>
